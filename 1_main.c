d4a6f20a07216e728a725e814843b614481dd43a
LMX169 <wftqnxvgn4@privaterelay.appleid.com>
2025-10-21 23:08:08 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,16 @@
-//
-//
-//
-#include <stdio.h>
+//202510304211
+//2124108137@qq.com
+//李景浩
+#include<stdio.h>
 int main()
 {
-    printf("Hello World");
+    float degree=0;
+    printf("请输入摄氏度\n");
+    scanf("%f",&degree);
+    float fahdegree=degree*5/9+32;
+    printf("它的华氏度为%f",fahdegree);
+
     return 0;
+
+
 }
