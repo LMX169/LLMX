c178e2ecb154313ab2cc00d4217285e536f5d63f
LMX169 <wftqnxvgn4@privaterelay.appleid.com>
2025-11-03 22:35:20 +0800

    4_3.4.c

--- 4_3.4.c
+++ 4_3.4.c
@@ -13,7 +13,7 @@ int power(int a,int b)
 }
 int main()
 {
-    int num = 1;
+    int num = 0;
     for (int i = 1; i <= 5; i++)
     {
         num +=power(i,2);
@@ -22,5 +22,6 @@ int main()
     return 0;
 
 }
+
 
 
